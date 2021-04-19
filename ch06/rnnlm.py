432f21c18c396b245aa2dc8023e7765909dcd8f6
Ahjeong Park <qkrdkwjd59@naver.com>
2021-04-19 16:57:59 +0900

    매개변수 읽기/쓰기를 처리하는 load_params(), save_params() 메서드 추가

--- ch06/rnnlm.py
+++ ch06/rnnlm.py
@@ -50,3 +50,11 @@ class Rnnlm(BaseModel):
 
     def reset_state(self):
         self.lstm_layer.reset_state()
+
+    def save_params(self, file_name = 'Rnnlm.pkl'):
+        with open(file_name, 'wb') as f:
+            pickle.dump(self.params, f)
+
+    def load_params(self, file_name = 'Rnnlm.pkl'):
+        with open(file_name, 'rb') as f:
+            self.params = pickle.load(f)
